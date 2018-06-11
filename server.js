d292dc6666d3b8c77f1bc3252fdbcf42b4b42b0f
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-06-11 14:02:58 +0000

    Add routes to get computes by id, networks and members.

--- server.js
+++ server.js
@@ -2,12 +2,53 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.json({
+    limit: '1mb'
+}));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port ' + 3000);
 });
 
 app.get('/compute', (req, res) => {
-    res.send([{id: 1, hostName: 'fakehostname', vCPU: 10, memory: 10, state: "FULLFIELD", localIpAddress: "127.0.0.1"}]);
+    res.send([{
+        id: 1,
+        hostName: 'fakehostname',
+        vCPU: 10,
+        memory: 10,
+        state: "FULLFIELD",
+        localIpAddress: "127.0.0.1"
+    }]);
+});
+
+app.get('/compute/:id', (req, res) => {
+    res.send({
+        id: 1,
+        hostName: 'fakehostname',
+        vCPU: 10,
+        memory: 10,
+        state: "FULLFIELD",
+        localIpAddress: "127.0.0.1"
+    });
+});
+
+app.get('/network', (req, res) => {
+    res.send([{
+        id: 2,
+        label: "label",
+        state: "FULLFIELD",
+        address: "127.0.0.1",
+        gateway: "129.0.0.1",
+        vLAN: "vlan",
+        networkAllocation: {
+            value: "value"
+        },
+        networkInterface: "interface",
+        MACInterface: "MACInterface",
+        interfaceState: "interfaceState"
+    }]);
+});
+
+app.get('/members', (req,res) => {
+    res.send(['id1', 'id2', 'id3']);
 });
